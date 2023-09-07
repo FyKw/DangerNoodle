4b06eba3d1eb6e10887df900036e30d6084ab938
Fynn K <Fynn.Kwast@btc-ag.com>
2023-09-07 15:26:38 +0200

    Structure cleanup part 3 added test_main.py without logical functionality

---
+++ test_main.py
@@ -0,0 +1,13 @@
+import unittest
+from main import _process_xml_file  # Replace with an actual function from your main.py
+
+
+class TestMainMethods(unittest.TestCase):
+
+    def test_process_xml_file(self):
+        # Todo
+        self.assertEqual(True, True)  # Replace with expected output
+
+
+if __name__ == '__main__':
+    unittest.main()
